100d452be2351a1aa9246a4ff75420abc76635de
Luis M. Diaz <ldiaz@redteam.com>
2023-04-25 15:19:07 -0500

    The new yor times update

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,158 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(NewsLetter());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NewsLetter extends StatelessWidget {
+  const NewsLetter({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: const Center(
+              child: Text(
+                'The New York Times',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontFamily: 'NewYorkTimes',
+                  fontSize: 26,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            leading: IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.blue[200],
+              ),
+              onPressed: () {},
             ),
-          ],
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.blue[200],
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Updated',
+                      style: TextStyle(
+                        color: Color(0xFFBECCD8),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      'FEBRUARY 11 at 19:23',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Image.network(
+                  'https://comunicaciones.congreso.gob.pe/wpuploads/2021/06/postulante-luis-carrasco.jpeg',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'PERU: "Approve initiative to strengthen the fight against bad business practices".',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 26,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '• The Consumer Defense Commission approved a proposal to strengthen leniency programs, a mechanism that allows the member of a business cartel to provide useful information to sanction infractions.',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '• Before the Agrarian Commission, the minister of the sector, Nelly Paredes, explains the execution of the resources for the reactivation program "Con Punche Perú Agro".',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Approve initiative to strengthen the fight against bad business practices',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 26,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '• The Consumer Defense Commission approved a proposal to strengthen leniency programs, a mechanism that allows the member of a business cartel to provide useful information to sanction infractions.',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '• The Culture Commission approved the bill that seeks to recognize June 23 as National Day of the Peruvian Theater, for the benefit of men and women who are dedicated to this activity.',
+                        style: TextStyle(
+                            fontFamily: 'PoltawskiNowy',
+                            color: Colors.black87,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
